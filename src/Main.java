deb2dd6b5cfc1c0aeb5c8fe41ffe92218bd59251
Aleksey Paremskiy <dragonoid845@gmail.com>
2023-01-12 23:38:20 +0700

    create

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException {
+
+    }
+}
